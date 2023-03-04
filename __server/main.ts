37a80e48d6f054e2efe5a4a3c5b60862a088d198
youkwhd <lolywk@tutanota.com>
2023-03-04 22:05:59 +0700

    chore: set expiresIn to be more faster

--- __server/main.ts
+++ __server/main.ts
@@ -30,12 +30,12 @@ app.post("/login", (req, res) => {
     const user: User | undefined = data.find((d) => d.username === req.body.username)
 
     if (!user)
-        return res.status(400).json({ err: null, content: null})
+        return res.status(400).json({ err: null, content: null })
 
     if (!bcrypt.compareSync(req.body.password, user.password))
-        return res.status(400).json({ err: null, content: null})
+        return res.status(400).json({ err: null, content: null })
 
-    user.token = jwt.sign(user, process.env.TOKEN_SECRET as string, { expiresIn: "1h" })
+    user.token = jwt.sign(user, process.env.TOKEN_SECRET as string, { expiresIn: "1m" })
     res.json({ err: null, content: { token: user.token }})
 })
 
