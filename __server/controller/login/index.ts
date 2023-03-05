6b6cac8d4d2583dfc0e7f4c1de0593e16de65c85
youkwhd <lolywk@tutanota.com>
2023-03-05 12:33:53 +0700

    fix: add error message

--- __server/controller/login/index.ts
+++ __server/controller/login/index.ts
@@ -11,10 +11,10 @@ router.post("/login", (req, res) => {
     const user: User | undefined = data.find((d) => d.username === req.body.username)
 
     if (!user)
-        return res.status(400).json({ err: null, content: null })
+        return res.status(400).json({ err: "User was not found", content: null })
 
     if (!bcrypt.compareSync(req.body.password, user.password))
-        return res.status(400).json({ err: null, content: null })
+        return res.status(400).json({ err: "Password incorrect", content: null })
 
     user.token = jwt.sign(user, process.env.TOKEN_SECRET as string, { expiresIn: "1m" })
     res.json({ err: null, content: { token: user.token }})
